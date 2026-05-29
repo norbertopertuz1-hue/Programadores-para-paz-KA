f1acf90e03423e13a6632bc995cc83b58e002ddb
norberto pertuz <norbertopertuz32@gmail.com>
2026-05-28 20:52:09 -0500

    actividad semana5 modificacion servidir

--- semana5/server.js
+++ semana5/server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const app = express();
 
@@ -18,4 +19,11 @@ app.get('/mensaje/:nombre', (req, res) => {
 
 app.listen(3000, () => {
   console.log('Servidor ejecutándose en puerto 3000');
+});
+app.post('/reporte', (req, res) => {
+  const mensaje = req.body.mensaje;
+  res.json({
+    estado: "Reporte recibido",
+    mensaje: mensaje
+  });
 });
